76c3fa72bd45e537fa38951cf7c5b6f9ee678031
han yong <hanyong_211@126.com>
2013-07-05 17:19:48 +0800

    hany

--- 8196C_EN/common.py
+++ 8196C_EN/common.py
@@ -4,7 +4,8 @@ Created on 2013-6-13
 @author: hany
 '''
 from robot.libraries import  BuiltIn
-
+import threading
+import time
 
 def process_special_charators(string):
     if string != None:
@@ -24,6 +25,19 @@ def string_lower(string):
 def remove_leading_space(string):
     return string.strip()
 
-        
+def async_open_browser(url,browser):
+    class browserThread(threading.Thread):
+        def __init__(self,url,browser):
+            threading.Thread.__init__(self)            
+            self.url = url
+            self.browser = browser
+
+        def run(self):
+             BuiltIn.BuiltIn().run_keyword('Open Browser',self.url,self.browser)
+             time.sleep(10)
+
+    pt = browserThread(url,browser)
+    pt.start()
+
 if __name__ == '__main__':
     pass
