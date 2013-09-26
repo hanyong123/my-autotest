15dd9269b15b291c88ec5ff119bf43668bcc4503
hany <hanyong_211@126.com>
2013-09-26 17:14:46 +0800

    ddsdf

---
+++ switch_auto_test/scapyLibary.py
@@ -0,0 +1,59 @@
+'''
+Created on 2013 9 3
+
+@author: hany
+
+robotframework scapy library
+'''
+import pexpect
+import time
+
+class scapySession:
+    def __init__(self):
+        self.name = None
+        self.handle = None
+
+class scapyLibary:
+    def __init__(self):
+        self.curSession = None
+        self.session_list = []
+        self.index = 0
+    
+    def open_scapy_session(self,name=None):
+        self.index = self.index + 1
+        self.curSession = pexpect.spawn('scapy')
+        time.sleep(2)
+        self.curSession.expect('>>>')
+        s = scapySession()
+        if name == None:
+            s.name = str(self.index)
+        else:
+            s.name = name
+        s.handle = self.curSession
+        self.session_list.append(s)
+    
+    def excute_scapy_conmmand(self,cmd):
+        self.curSession.sendline(cmd)
+        self.curSession.expect('>>>')
+        return self.curSession.before
+    
+    
+    def switch_session(self,name):
+        for s in self.session_list:
+            if s.name == str(name):
+                self.curSession = s.handle
+                break
+    
+    def send_ctrl_c(self):
+        self.curSession.sendcontrol('c')
+        self.curSession.expect('>>>')
+        return self.curSession.before 
+        
+                        
+if __name__ == '__main__':
+    l = scapyLibary()
+    l.open_scapy_session()
+    s = l.excute_scapy_conmmand("p = IP(src='192.168.1.3',dst='192.168.1.1')/ICMP()/'aaaaaaaaaaaaaaaaaaaaaa'")
+    print s
+    s = l.excute_scapy_conmmand('send(p)')
+    print s
